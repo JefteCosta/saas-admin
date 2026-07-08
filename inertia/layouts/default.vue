230c2220d367049a3f88a05f62861a3740c2e3fa
Jefte Costa <jefte@meetrox.ai>
2026-07-08 03:27:08 +0000

    fix: adicionar slot no layout default para renderizar páginas

--- inertia/layouts/default.vue
+++ inertia/layouts/default.vue
@@ -1,9 +1,3 @@
-<script lang="ts">
-export const description = 'A sidebar that collapses to icons.'
-export const iframeHeight = '800px'
-export const containerClass = 'w-full h-full'
-</script>
-
 <script setup lang="ts">
 import AppSidebar from '~/components/app_sidebar.vue'
 import { SidebarInset, SidebarProvider } from '~/components/ui/sidebar'
@@ -15,13 +9,8 @@ import SiteHeader from '~/components/site_header.vue'
     <AppSidebar />
     <SidebarInset>
       <SiteHeader />
-      <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div class="bg-muted/50 aspect-video rounded-xl" />
-          <div class="bg-muted/50 aspect-video rounded-xl" />
-          <div class="bg-muted/50 aspect-video rounded-xl" />
-        </div>
-        <div class="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+      <div class="flex flex-1 flex-col gap-4 p-4 pt-4">
+        <slot />
       </div>
     </SidebarInset>
   </SidebarProvider>
